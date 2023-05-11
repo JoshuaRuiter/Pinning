cda8dd8b294aa19712d73fdcabef98aa1926f528
JoshuaRuiter <jruiter@comcast.net>
2023-05-11 09:58:29 -0500

    No significant changes

--- Temp.m
+++ Temp.m
@@ -7,38 +7,38 @@
 % RunSLTests(4)
 % RunSLTests(5)
 % RunSOTests(4,1)
-RunSUTests(4,2)
+% RunSUTests(4,2)
 
 %%% Root system axioms
-for n=2:10
-    A_n = RootSystem('A',n);
-    A_n.VerifyProperties();
-
-    B_n = RootSystem('B',n);
-    B_n.VerifyProperties();
-
-    C_n = RootSystem('C',n);
-    C_n.VerifyProperties();
-    
-    BC_n = RootSystem('BC',n);
-    BC_n.VerifyProperties();
-
-    if n >= 4
-        D_n = RootSystem('D',n);
-        D_n.VerifyProperties();
-    end
-end
-
-for n=6:8
-    E_n = RootSystem('E',n,8);
-    E_n.VerifyProperties();
-end
-
-G_2 = RootSystem('G',2,3);
-G_2.VerifyProperties();
-
-F_4 = RootSystem('F',4,4);
-F_4.VerifyProperties();
+% for n=2:10
+%     A_n = RootSystem('A',n);
+%     A_n.VerifyProperties();
+% 
+%     B_n = RootSystem('B',n);
+%     B_n.VerifyProperties();
+% 
+%     C_n = RootSystem('C',n);
+%     C_n.VerifyProperties();
+%     
+%     BC_n = RootSystem('BC',n);
+%     BC_n.VerifyProperties();
+% 
+%     if n >= 4
+%         D_n = RootSystem('D',n);
+%         D_n.VerifyProperties();
+%     end
+% end
+% 
+% for n=6:8
+%     E_n = RootSystem('E',n,8);
+%     E_n.VerifyProperties();
+% end
+% 
+% G_2 = RootSystem('G',2,3);
+% G_2.VerifyProperties();
+% 
+% F_4 = RootSystem('F',4,4);
+% F_4.VerifyProperties();
 
 %%%%%%%%%%%%%%%%%%%%%%%
 %% THESE DO NOT PASS %%
@@ -51,10 +51,10 @@ F_4.VerifyProperties();
 %%%   - two medium roots
 %%%   - beta long, alpha medium
 %%%   - beta medium, alpha long
-RunSUTests(6,3)
+% RunSUTests(6,3)
 % RunSUTests(8,4)
 % RunSUTests(10,5)
 
 %%% Goals to work towards
-% RunSOTests(6,2)
-RunSUTests(6,2)
+RunSOTests(6,2)
+% RunSUTests(6,2)
